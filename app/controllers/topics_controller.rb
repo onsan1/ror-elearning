cfe60c0695523f9624c8396b72d271b45cf1fc1d
onsan1 <ondaproduction1008@gmail.com>
2018-05-16 11:35:31 +0800

    Re Edit Boostrap

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,7 @@
 class TopicsController < ApplicationController
- before_action :set_topic, only:[:show, :edit, :update, :destroy]
-
-  def index
-  	  @topics = Topic.all
-  end
-
-  def show
-  end
 
   def new
-  	@topic = Topic.new
+  	
   end
 
-  def edit
-
-  end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,4 @@ class UsersController < ApplicationController
 		
 	end
 
-	def show
-		
-	end
 end
