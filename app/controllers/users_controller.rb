9ac62d45b0f0b3c25472b71bb6b935803674e8ca
onsan1 <ondaproduction1008@gmail.com>
2018-05-18 11:56:48 +0800

    Reedit Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,19 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcom to the Sample App"
-      redirect_to @user
-    else
-      render 'new'
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcom to the Sample App"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
-  end
 
-  private
-   def user_params
-     params.require(:user).permit(:name, :email, :password, :password_confirmation)    
-   end
+    private
+      def user_params
+       params.require(:user).permit(:name, :email, :password, :password_confirmation)    
+      end
 end
 
